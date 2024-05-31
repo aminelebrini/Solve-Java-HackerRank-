a4b2c197b4727a30f08921738004849348adf5f8
Lebrini Amine <aminelebrini4@gmail.com>
2024-05-31 20:10:05 +0100

    'update'

---
+++ Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+       String myString = scan.next();
+       int t = scan.nextInt();
+       int x = scan.nextInt();
+       scan.close();
+       
+       System.out.println(myString);
+       System.out.println(t);
+       System.out.println(x);
+    }
+}
