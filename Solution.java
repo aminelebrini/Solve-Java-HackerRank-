af72df66e7cfcddb2f1e423989bf0ce8b69bf4fa
AMINE LEBRINI <113290995+aminelebrini@users.noreply.github.com>
2024-08-27 11:21:05 +0100

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
        String myString = scan.next();
-       int n = scan.nextInt();
+       int x = scan.nextInt();
        int t = scan.nextInt();
        scan.close();
        
